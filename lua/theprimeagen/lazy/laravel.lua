8ef10b4881e963df199b70a80ebea14cf86c29cb
André Azevedo <andre.azev@gmail.com>
2025-03-11 20:23:33 -0300

    feat: made laravel plugin load conditionally;

--- lua/theprimeagen/lazy/laravel.lua
+++ lua/theprimeagen/lazy/laravel.lua
@@ -1,5 +1,12 @@
 return {
     "adalessa/laravel.nvim",
+    cond = function()
+        -- Check if PHP is available in the system
+        local php_installed = vim.fn.executable("php") == 1
+        -- Check if a composer.json file exists in the current working directory
+        local composer_exists = vim.fn.filereadable(vim.fn.getcwd() .. "/composer.json") == 1
+        return php_installed and composer_exists
+    end,
     dependencies = {
         "tpope/vim-dotenv",
         "nvim-telescope/telescope.nvim",
@@ -19,11 +26,11 @@ return {
                 enable = true,
             },
             route_info = {
-                enable = true, --- to enable the laravel.nvim virtual text
+                enable = true,      --- to enable the laravel.nvim virtual text
                 position = 'right', --- where to show the info (available options 'right', 'top')
                 middlewares = true, --- wheather to show the middlewares section in the info
-                method = true, --- wheather to show the method section in the info
-                uri = true  --- wheather to show the uri section in the info
+                method = true,      --- wheather to show the method section in the info
+                uri = true          --- wheather to show the uri section in the info
             },
         },
     },
